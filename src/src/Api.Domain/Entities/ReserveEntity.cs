b234085acce1140305accbc0ca5fd1f57369ed74
Thiago Augusto <taugusto964@gmail.com>
2024-04-09 13:37:22 -0300

    Criação branch reservas

---
+++ src/src/Api.Domain/Entities/ReserveEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Domain.Entities
+{
+    public class ReserveEntity:BaseEntity
+    {
+        public string ReserveCode {  get; set; }
+        public ReserveStatus ReserveStatus { get; set; }
+        public bool ExtraLuggage {  get; set; }
+        public Guid CustomerId {  get; set; }
+        public Guid FlightId {  get; set; }
+    }
+    public enum ReserveStatus
+    {
+        NotConfirmed = 0, Confirmed = 1
+    }
+}
