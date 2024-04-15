5d4817af9eeecbf8678388c27cf1fd29dddfc3b8
geocadorin <geeo.cadorin@gmail.com>
2024-04-15 19:22:29 -0300

    started pruchase aggregate

---
+++ src/src/Api.Domain/Dtos/PaymentAggregate/PaymentCreateDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Dtos.PaymentAggregate
+{
+    public class PaymentCreateDto
+    {
+        [Required(ErrorMessage = "C é um campo obrigatório")]
+        [StringLength(60, ErrorMessage = "Sobrenome deve ter no máximo {1} caracteres.")]
+        public string PaymentCode { get; set; }
+
+    }
+}
---
+++ src/src/Api.Domain/Entities/PurchaseAggregate/PaymentEntity.cs
@@ -0,0 +1,34 @@
+﻿using Api.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.PaymentAggregate
+{
+    public class PaymentEntity : BaseEntity
+    {
+        public Guid? OfferId { get; set; }
+        public string PaymentCode { get; set; }
+        public StatusPayment StatusId { get; set; }
+        public TypesPayment TypePayment { get; set; }
+        public decimal SubTotal { get; set; }
+        public decimal Total { get; set; }
+    }
+
+    public enum TypesPayment
+    {
+        Cash = 1,
+        CreditCard = 2,
+        DebitCard = 3,
+        Pix = 4
+    }
+
+    public enum StatusPayment
+    {
+        Active = 1,
+        Inactive = 2,
+        Blocked = 3
+    }
+}
