1960b3ddfaf26cb9694ea0732ea4ceb53c754261
Moises Meireles <30697342+moisesmeireles@users.noreply.github.com>
2024-06-11 19:11:06 -0300

    update_Axios

--- frontend/src/axios/axiosConfig.js
+++ frontend/src/axios/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseurl = 'https://uaifly.azurewebsites.net'
+const baseurl = 'https://uaifly-server-test.azurewebsites.net'
 
 const api = axios.create({
     baseURL: baseurl, // Substitua pela URL base da sua API
